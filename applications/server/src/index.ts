9045ed2338268b81c01f020fc9be2fdad32315cf
Gorobec Ivan <work.grorbec@gmail.com>
2024-12-21 04:30:49 +0500

    server: add build url to CORS pass

--- applications/server/src/index.ts
+++ applications/server/src/index.ts
@@ -11,6 +11,7 @@ setupSwagger(app);
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
